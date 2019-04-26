3c4938e5b2010b147530706f40611c7b1ba95dbf
Soumitra Pal <soumitrakp@gmail.com>
2019-04-26 18:12:20 -0400

    now pip packages are properly built, uploaded and installed

---
+++ judi/__init__.py
@@ -0,0 +1,4 @@
+from .judi import add_param, Task, File, combine_csvs
+from .version import __version__
+__all__ = ['add_param', 'Task', 'File', 'combine_csvs']
+name = 'JUDI'
--- setup.py
+++ setup.py
@@ -1,19 +1,28 @@
-import setuptools
+import setuptools, re
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+VERSIONFILE = "judi/version.py"
+verstrline = open(VERSIONFILE, "rt").read()
+VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
+mo = re.search(VSRE, verstrline, re.M)
+if mo:
+    verstr = mo.group(1)
+else:
+    raise RuntimeError("Unable to find version string in %s." % (VERSIONFILE))
+
 setuptools.setup(
-     name='judi',  
-     version='0.1',
-     author="Soumitra Pal",
-     author_email="soumitrakp@gmail.com",
-     description="A Software Workflow Management System Based on DoIt",
-     long_description=long_description,
-     long_description_content_type="text/markdown",
-     url="https://github.com/ncbi/JUDI",
-     packages=setuptools.find_packages(),
-     classifiers=[
+     name = 'judi',  
+     version = verstr,
+     author = "Soumitra Pal",
+     author_email = "soumitrakp@gmail.com",
+     description = "A Software Workflow Management System Based on DoIt",
+     long_description = long_description,
+     long_description_content_type = "text/markdown",
+     url = "https://github.com/ncbi/JUDI",
+     packages = setuptools.find_packages(),
+     classifiers = [
          "Programming Language :: Python :: 3",
          "License :: OSI Approved :: MIT License",
          "Operating System :: OS Independent",
