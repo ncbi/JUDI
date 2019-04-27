5dfb20d543be6fef36c59b81d2c9ac4d544964de
Soumitra Pal <soumitrakp@gmail.com>
2019-04-27 17:09:46 -0400

    fixed an error to build readthedocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2019, Soumitra Pal'
 author = 'Soumitra Pal'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1'
+release = '0.0.1'
 
 
 # -- General configuration ---------------------------------------------------
@@ -53,3 +53,5 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+master_doc = 'index'
